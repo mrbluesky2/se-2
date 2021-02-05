c377e86e498c692d3787464dd6e73fd1b5d04c8b
mrbluesky2 <pierrecarr2@gmail.com>
2021-02-04 22:31:39 -0800

    zero division test

--- unit-test/unit_test.py
+++ unit-test/unit_test.py
@@ -10,25 +10,28 @@ def multiplication(a, b):
     return a * b
     
 def division(a, b):
-    if b == 0: return 0
-    else: return a / b
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return ZeroDivisionError
 
 class testArithmetic(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(a.addition(6, 2), 8)
-        self.assertEqual(a.addition(12, 4), 16)
+        self.assertEqual(addition(6, 2), 8)
+        self.assertEqual(addition(12, 4), 16)
     
     def test_subtraction(self):
-        self.assertEqual(a.subtraction(6, 2), 4)
-        self.assertEqual(a.subtraction(12, 4), 8)
+        self.assertEqual(subtraction(6, 2), 4)
+        self.assertEqual(subtraction(12, 4), 8)
 
     def test_division(self):
-        self.assertEqual(a.division(6, 2), 3)
-        self.assertEqual(a.division(12, 3), 4)
+        self.assertEqual(division(1, 0), ZeroDivisionError)
+        self.assertEqual(division(6, 2), 3)
+        self.assertEqual(division(12, 3), 4)
 
     def test_multiplication(self):
-        self.assertEqual(a.multiplication(6, 2), 12)
-        self.assertEqual(a.multiplication(12, 4), 48)
+        self.assertEqual(multiplication(6, 2), 12)
+        self.assertEqual(multiplication(12, 4), 48)
 
 if __name__ == '__main__':
     unittest.main()
