35fb2e0a94de84443f78d85dc736a037a4bbe001
Pierre Carr <pierrecarr2@gmail.com>
2021-03-08 22:22:45 -0800

    impli. fb_value method to calc fizzbuzz for value

--- hw-7/fizzbuzz.py
+++ hw-7/fizzbuzz.py
@@ -1,2 +1,12 @@
+# input: int value
+# output: 'Fizz' and or 'Buzz'
 def fb_value(value):
-    return 'FB'
+    return 'Fizz' * (value % 3 < 1) + 'Buzz' * (value % 5 < 1)
+
+# input: none
+# output: 1-100 and their fizzbuzz string
+def fizzbuzz():
+    for i in range(100):
+        print( '[{}]: {}'.format(i, fb_value(i)) )
+
+fizzbuzz()
