93e862dbf58b87843b3e094c1ead015fd13fb4aa
Pierre Carr <pierrecarr2@gmail.com>
2021-03-08 22:24:53 -0800

    impli. leap_year method for calc if input is_leap

--- hw-7/leapyear.py
+++ hw-7/leapyear.py
@@ -1,3 +1,14 @@
+# input: int year
+# output: bool is_leap
 def leap_year(year):
     ret = False
+    # every 4 years
+    if year % 4 == 0:
+        # unless it's a mult. of 100
+        if year % 100 == 0:
+            # unless it's a mult. of 400
+            if year % 400 == 0:
+                ret = True
+        else:
+            ret = True
     return ret
